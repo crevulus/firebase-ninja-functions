724c58b04a603288efc3329f718938c98be8b005
crevulus <crevulus@gmail.com>
2021-09-12 18:41:55 +0200

    complete upvote fn & implementation

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ exports.handleUserDeleted = functions.auth.user().onDelete((user) => {
 
 exports.addRequest = functions.https.onCall((data, context) => {
   const text = data.text;
-  // hass access to whether or not user is logged in
+  // has access to whether or not user is logged in
   if (!context.auth) {
     // fb has a series of error codes to pass in as first arg; second arg is to show on front end
     throw new functions.https.httpsErrorInstance(
@@ -29,11 +29,40 @@ exports.addRequest = functions.https.onCall((data, context) => {
   if (text.length > 30) {
     throw new functions.https.httpsErrorInstance(
       "invalid-argument",
-      "Request must be equal to or fewer than 30 characters"
+      "Request must be equal to or fewer than 30 characters."
     );
   }
   return admin.firestore().collection("requests").add({
     text,
     upvotes: 0,
+  });
+});
+
+exports.upvoteRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.httpsErrorInstance(
+      "unauthenticated",
+      "Only authenticated users can upvote requests!"
+    );
+  }
+  // get references for two docs: user and request
+  const user = admin.firestore().collection("users").doc(context.auth.uid);
+  const request = admin.firestore().collection("requests").doc(data.id); // we will pass the id of the tut req when we call the fn
+  return user.get().then((doc) => {
+    // check user hasn't already upvoted this request
+    if (doc.data().upvotedOn.includes(data.id)) {
+      throw new functions.https.httpsErrorInstance(
+        "failed-precondition",
+        "Can only upvote something once."
+      );
+    }
+    // update upvotedOn array
+    user.update({
+      upvotedOn: [...doc.data().upvotedOn, data.id],
+    });
+    request.update({
+      upvotes: admin.firestore.FieldValue.increment(1),
+    });
+    return;
   });
 });
--- public/js/requests.js
+++ public/js/requests.js
@@ -1,10 +1,21 @@
-var app = new Vue({
-  el: "#app",
+const list = new Vue({
+  el: "#list",
   data: {
     requests: [],
   },
+  methods: {
+    upvoteRequest(id) {
+      const upvote = firebase.functions().httpsCallable("upvoteRequest");
+      upvote({ id }).catch((error) => {
+        console.log(error.message);
+      });
+    },
+  },
   mounted() {
-    const ref = firebase.firestore().collection("requests");
+    const ref = firebase
+      .firestore()
+      .collection("requests")
+      .orderBy("upvotes", "desc");
     ref.onSnapshot((snapshot) => {
       let requests = [];
       snapshot.forEach((doc) => {
