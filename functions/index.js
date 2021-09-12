53689d8e19e36cfe5ff5ebfc262f8dd3159cd7e8
crevulus <crevulus@gmail.com>
2021-09-12 13:40:09 +0200

    reverted to net ninja's files

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+  response.redirect('https://www.thenetninja.co.uk');
+});
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+  const name = data.name;
+  return `hello ${name} :)`;
+});
